7705e4d71c3cb67c844c4a7d6e7c13335c040bd6
Luan <luan.nascimento.dev@gmail.com>
2025-05-23 16:03:04 -0300

    fix: port server workflow

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 const app = express();
 
-const port = process.env.PORT ?? 8080;
+const port = Number(process.env.PORT) || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${String(port)}`);
 });
